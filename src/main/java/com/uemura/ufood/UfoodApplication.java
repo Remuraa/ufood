4bcfffb11973a51a1077dbfbe5f38ac10c90e6db
renan <renan.uemura@logicsp.com.br>
2020-11-03 21:16:25 -0300

    Criacao projeto

---
+++ src/main/java/com/uemura/ufood/UfoodApplication.java
@@ -0,0 +1,13 @@
+package com.uemura.ufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uemura/ufood/UfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uemura.ufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
